f750606f403f2dc489b3480c3cf6cdcc9a6d0356
diqkraud <daesan12>
2024-11-20 15:41:29 +0900

    feat: CreateBoardRequestDto오류 수정

--- src/main/java/diary/requestDto/CreateBoardRequestDto.java
+++ src/main/java/diary/requestDto/CreateBoardRequestDto.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 
 @Getter
 public class CreateBoardRequestDto {
-    private Long
+    private final Long userId;
     private final String title;
     private final String content;
     private final String weather;
 
-    public CreateBoardRequestDto(String title, String content, String weather) {
+    public CreateBoardRequestDto(Long userId, String title, String content, String weather) {
+        this.userId = userId;
         this.title = title;
         this.content = content;
         this.weather = weather;
