3231df52d64307ac8cca35301b6e8b97987edd3c
kimdanbin <rlaeksqls@naver.com>
2024-11-20 15:39:07 +0900

    User 엔티티 생성자 주입 추가

--- src/main/java/diary/entity/User.java
+++ src/main/java/diary/entity/User.java
@@ -27,4 +27,10 @@ public class User  extends Base{
     public User(){
 
     }
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 }
