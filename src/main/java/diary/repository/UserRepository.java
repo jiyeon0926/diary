dcbcef1b2f95132af1249f5ffe73088443ac826f
kimdanbin <rlaeksqls@naver.com>
2024-11-20 17:42:50 +0900

    feat: 회원가입 기능 (모든 예외처리 추가)

--- src/main/java/diary/repository/UserRepository.java
+++ src/main/java/diary/repository/UserRepository.java
@@ -2,6 +2,11 @@ package diary.repository;
 
 import diary.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 중복 확인 (이 메소드는 JPA가 자동으로 구현)
+    boolean existsByEmail(String email);
 }
