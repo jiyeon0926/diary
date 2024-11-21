203dc0960051881807872a4d55cf86b89654269a
memo <colorlhs@gmail.com>
2024-11-21 11:09:13 +0900

    feat: Board Entity에 good(좋아요) 추가

--- src/main/java/diary/responseDto/BoardResponseDto.java
+++ src/main/java/diary/responseDto/BoardResponseDto.java
@@ -9,13 +9,15 @@ public class BoardResponseDto {
     private final String title;
     private final String content;
     private final String weather;
+    private final Long good;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
-    public BoardResponseDto(Long id, String title, String content, String weather,LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public BoardResponseDto(Long id, String title, String content, String weather, Long good, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id= id;
         this.title= title;
         this.content = content;
         this.weather = weather;
+        this.good = good;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
     }
@@ -25,6 +27,7 @@ public class BoardResponseDto {
                 board.getTitle(),
                 board.getContent(),
                 board.getWeather(),
+                board.getGood(),
                 board.getCreatedAt(),
                 board.getModifiedAt()
         );
