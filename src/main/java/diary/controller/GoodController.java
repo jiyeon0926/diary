86d47817102ef4dffded948689d4e3f3f2a765be
memo <colorlhs@gmail.com>
2024-11-21 14:39:07 +0900

    feat:controller issue

--- src/main/java/diary/controller/GoodController.java
+++ src/main/java/diary/controller/GoodController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GoodController {
-    @PostMapping("/board/{id}")
-    public ResponseEntity<GoodResponseDto> good(@PathVariable Long id, @RequestBody GoodRequestDto goodRequestDto){
-
-        return ResponseEntity<>(goodRequestDto);
-    }
-}
+//@RestController
+//public class GoodController {
+//    @PostMapping("/board/{id}")
+//    public ResponseEntity<GoodResponseDto> good(@PathVariable Long id, @RequestBody GoodRequestDto goodRequestDto){
+//
+//        return ResponseEntity<>(goodRequestDto);
+//    }
+//}
