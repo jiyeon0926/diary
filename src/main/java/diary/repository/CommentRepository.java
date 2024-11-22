1b132db7ef393912d5686a120dbc9cb013af4078
Yeon <yeoni9094@naver.com>
2024-11-22 15:25:53 +0900

    refactor: 내림차순 수정 및 오류메세지 정정
    - ProfileRepository의 findUserByidOrElseThrow 메서드는 다른 기능에서도 가져와서 사용하기 때문에 공통으로 알 수 있는 오류 메세지로 정정
    - 회원가입을 먼저 등록하라는 오류 메세지가 아닌, 해당 아이디가 없다는 오류 메세지로 정정

--- src/main/java/diary/repository/CommentRepository.java
+++ src/main/java/diary/repository/CommentRepository.java
@@ -18,7 +18,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     }
 
     // 수정일 내림차순 조회
-    List<Comment> findAllByBoardIdOrderByModifiedAt(Long boardId);
+    List<Comment> findAllByBoardIdOrderByModifiedAtDesc(Long boardId);
 
     @Query("SELECT c FROM Comment c INNER JOIN Board b ON c.board.id = :boardId WHERE (c.user.id = :userId OR b.user.id = :writerId) AND c.id = :commentId")
     Comment findCommentByBoardIdAndUserId(@Param("boardId") Long boardId, @Param("userId") Long userId, @Param("writerId") Long writerId, @Param("commentId") Long commentId);
