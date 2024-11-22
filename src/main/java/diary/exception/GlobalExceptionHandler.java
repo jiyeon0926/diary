af672a712f42035d235a13cf3f8943a5392097c7
memo <colorlhs@gmail.com>
2024-11-22 20:23:47 +0900

    feat: 개행 삭제

--- src/main/java/diary/exception/GlobalExceptionHandler.java
+++ src/main/java/diary/exception/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
-package diary.Exception;
+package diary.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
