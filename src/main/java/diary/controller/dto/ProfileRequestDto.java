809ff7e7046a92ee3278173de2c28bfbd11f0aec
kimdanbin <rlaeksqls@naver.com>
2024-11-23 21:17:00 +0900

    fix: 리퀘스트바디의 id 제거

--- src/main/java/diary/controller/dto/ProfileRequestDto.java
+++ src/main/java/diary/controller/dto/ProfileRequestDto.java
@@ -4,12 +4,10 @@ import lombok.Getter;
 
 @Getter
 public class ProfileRequestDto {
-    private final Long id;
     private final String password;
     private final String username;
 
-    public ProfileRequestDto(Long id, String password, String username) {
-        this.id = id;
+    public ProfileRequestDto(String password, String username) {
         this.password = password;
         this.username = username;
     }
